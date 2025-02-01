10228db6c93cec01890023d28a91a2ff5fd95f78
Felipe Bispo <134074800+0FelipeBisp0@users.noreply.github.com>
2025-02-01 14:40:52 -0300

    Questão 5.js

--- Desafio 1/Questão 5.js
+++ Desafio 1/Questão 5.js
@@ -1,12 +1,17 @@
 /* Crie uma variável idade e atribua um valor. Se a pessoa for maior de idade (18 ou mais), exiba "Você é maior de idade". Caso contrário, exiba "Você é menor de idade". */
 
-let idade = Number(prompt("Digite a idade:")); 
+let idade;
 
+do {
+    idade = Number(prompt("Digite a idade:"));
+    
+    if (isNaN(idade) || idade < 0) {
+        console.log("Por favor, insira uma idade válida.");
+    }
+} while (isNaN(idade) || idade < 0);
 
 if (idade >= 18) {
     console.log("Você é maior de idade!");
-
 } else {
-    console.log("Você é menor de idade!")
-
+    console.log("Você é menor de idade!");
 }
