b578af454622bf881bbc23885f0f2313a6dc5c1b
Felipe Bispo <134074800+0FelipeBisp0@users.noreply.github.com>
2025-02-01 14:41:47 -0300

    Update Questão 6.js

--- Desafio 1/Questão 6.js
+++ Desafio 1/Questão 6.js
@@ -5,7 +5,7 @@ let nota = Number(prompt("Digite a nota:"));
 
 if (nota >= 7) {
     console.log("Aprovado");
-} else if (nota >= 5 && nota <= 6) {
+} else if (nota >= 5) {
     console.log("Recuperação");
 } else {
     console.log("Reprovado");
